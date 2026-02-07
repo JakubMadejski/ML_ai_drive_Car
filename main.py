bd8d04cc1063a0623e8a4182212fed3e03337024
JakubMadejski <01206793@pw.edu.pl>
2026-02-07 14:46:42 +0100

    trasa - wczytywanie z pliku

--- main.py
+++ main.py
@@ -1,65 +1,104 @@
 import pygame
 import math
 
-WIDTH, HEIGHT = 1600, 1200
+WIDTH, HEIGHT = 1200, 800
 CAR_SIZE_X, CAR_SIZE_Y = 30, 60
 BORDER_COLOR = (255, 255, 255)
 
-
+#kolory
+GRASS_CLOR = (34, 177, 76)
+ROAD_COLOR = (50, 50, 50)
+CAR_COLOR = (200, 0, 0)
 
 class Car:
     def __init__(self):
-        self.x = WIDTH / 2
-        self.y = HEIGHT / 2
+        self.start_pos = (150, 200)
+        self.x, self.y = self.start_pos
         self.angle = 0
         self.speed = 5
+        self.alive = True
     
     def move(self):
+        if not self.alive: 
+            return
         keys = pygame.key.get_pressed()
-
+        if keys[pygame.K_LEFT]: self.angle += 3
+        if keys[pygame.K_RIGHT]: self.angle -= 3
         if keys[pygame.K_UP]:
             rad = math.radians(self.angle)
-            self.x -= self.speed * math.sin(rad)
-            self.y -= self.speed * math.cos(rad)
-        
-        if keys[pygame.K_LEFT]:
-            self.angle += 5
-        
-        if keys[pygame.K_RIGHT]:
-            self.angle -= 5
+            self.x -= math.sin(rad) * self.speed
+            self.y -= math.cos(rad) * self.speed
 
-        if self.x < 20: self.x = 20
-        if self.x > WIDTH - 20: self.x = WIDTH - 20
-        if self.y < 20: self.y = 20
-        if self.y > HEIGHT - 20: self.y = HEIGHT - 20
+    def check_collision(self):
+        corners = self.get_corners()
+
+        for corner in corners:
+                try:
+                    pixel_color = screen.get_at((int(corner[0]), int(corner[1])))[:3]
+                    if pixel_color[:3] != ROAD_COLOR:
+                        self.reset()
+                        break
+                except IndexError:
+                    self.reset()
+                    break
+    
+    def get_corners(self):
+        corners = []
+
+        rad = math.radians(self.angle)
+
+        length = CAR_SIZE_Y / 2
+        width = CAR_SIZE_X / 2
+
+        for sx, sy in [(-width, -length), (width, -length), (width, length), (-width, length)]:
+            rotated_x = self.x + (sx * math.cos(-rad) - sy * math.sin(-rad))
+            rotated_y = self.y + (sx * math.sin(-rad) + sy * math.cos(-rad))
+            corners.append((rotated_x, rotated_y))
+        return corners
+
+    def reset(self):
+        self.x, self.y = self.start_pos
+        self.angle = 0
+        self.alive = True
+
 
     def draw(self, screen):
         car_surface = pygame.Surface((CAR_SIZE_X, CAR_SIZE_Y), pygame.SRCALPHA)
         
-        pygame.draw.rect(car_surface, (200, 0, 0), (0, 0, CAR_SIZE_X, CAR_SIZE_Y))
+        pygame.draw.rect(car_surface, CAR_COLOR, (0, 0, CAR_SIZE_X, CAR_SIZE_Y))
         
         pygame.draw.rect(car_surface, (0, 0, 255), (5, 5, 20, 10)) 
         
         rotated_surface = pygame.transform.rotate(car_surface, self.angle)
-        rect = rotated_surface.get_rect(center=(self.x, self.y))
+        rect = rotated_surface.get_rect(center=(int(self.x), int(self.y)))
         
         screen.blit(rotated_surface, rect.topleft)
 
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
+
+try:
+    track_image = pygame.image.load('track.png')
+    track_image = pygame.transform.scale(track_image, (WIDTH, HEIGHT))
+except:
+    print("Nie można załadować obrazu trasy. Upewnij się, że 'track.png' jest w tym samym folderze co skrypt.")
+    pygame.quit()
+    exit()
+
 car = Car()
 
 running = True
 while running:
-    screen.fill((0, 0, 0))
+    screen.blit(track_image, (0, 0)) # Rysowanie tła
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    car.move()
-    car.draw(screen)
+    car.move() # Logika gry
+    car.check_collision() # Sprawdzenie kolizji
+    car.draw(screen) #Rrysowanie samochodu 
 
     pygame.display.flip()
     clock.tick(60)
